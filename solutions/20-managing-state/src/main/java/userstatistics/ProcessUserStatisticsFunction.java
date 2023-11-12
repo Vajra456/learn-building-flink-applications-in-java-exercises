a9a226ab869f0b5cbb850ea5707cb20dc3e8797a
Vajra456 <41577630+Vajra456@users.noreply.github.com>
2023-11-12 10:03:49 +0530

    Update ProcessUserStatisticsFunction.java

--- solutions/20-managing-state/src/main/java/userstatistics/ProcessUserStatisticsFunction.java
+++ solutions/20-managing-state/src/main/java/userstatistics/ProcessUserStatisticsFunction.java
@@ -11,14 +11,22 @@ import models.UserStatistics;
 class ProcessUserStatisticsFunction extends ProcessWindowFunction<UserStatistics, UserStatistics, String, TimeWindow> {
     private ValueStateDescriptor<UserStatistics> stateDescriptor;
 
+    // Initializes the Descriptor
     @Override
     public void open(Configuration parameters) throws Exception {
         stateDescriptor = new ValueStateDescriptor<>("User Statistics", UserStatistics.class);
         super.open(parameters);
     }
 
+    // Takes three paramters
+    // key of the record
+    // Context to get the sate. State is used to get the accumulated state
+    // List of stats accumulated over a period of time
+    // collector is used to emit the result
     @Override
     public void process(String emailAddress, ProcessWindowFunction<UserStatistics, UserStatistics, String, TimeWindow>.Context context, Iterable<UserStatistics> statsList, Collector<UserStatistics> collector) throws Exception {
+        // The ValueState is used to maintain the state for a specific key across different processing windows.
+        // key is implicitly associated with the ValueState object obtained from the context.globalState()
         ValueState<UserStatistics> state = context.globalState().getState(stateDescriptor);
         UserStatistics accumulatedStats = state.value();
 
